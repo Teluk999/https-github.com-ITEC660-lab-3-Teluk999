fd88f15f2106cc467da64993f50b05e5f95a5276
Teluk999 <147182078+Teluk999@users.noreply.github.com>
2023-11-12 14:07:23 +0300

    My vue js app

---
+++ App.vue
@@ -0,0 +1,27 @@
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+import Home from './views/Home.vue'
+</script>
+
+<template>
+  <div class="cont">
+  <header>
+    <h1>Musixmatch Web API App</h1>
+  </header>
+
+  <main>
+    <!--<TheWelcome />-->
+    <Home/>
+  </main>
+</div>
+</template>
+
+<style scoped>
+.cont {
+  font-size: 20px;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+}
+</style>
